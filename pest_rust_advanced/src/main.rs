2267bf26fde81a03a9f8aa7cc38aea9f40f36644
Matt <matthewneaveis@hotmail.co.uk>
2024-01-10 11:14:23 +0000

    Basic parsing of advanced pest structure

--- pest_rust_advanced/src/main.rs
+++ pest_rust_advanced/src/main.rs
@@ -2,7 +2,6 @@ use std::fs;
 use pest::Parser;
 use pest_derive::Parser;
 use pest::iterators::Pair;
-use pest::iterators::Pairs;
 
 #[derive(Parser)]
 #[grammar = "elixir_ast_v1.pest"]
@@ -13,12 +12,64 @@ fn main() {
     let file_content = fs::read_to_string(path)
         .expect(format!("Failed to read {}", path).as_str());
 
-
-    let test_ast = ASTParser::parse(Rule::condensed_list, "'hello'");
-    println!("{:?}", test_ast);
-
-    let prog_ast = ASTParser::parse(Rule::defmodule, file_content.as_str());
-    println!("{:?}", prog_ast);
+    let prog_ast = ASTParser::parse(Rule::defmodule, file_content.as_str())
+        .expect("Failed to parse the AST")
+        .next()
+        .unwrap();
+    
+    // Match prog_ast on Rule::defmodule
+    // prog_ast.into_inner() returns [comma, metadata, comma, alias, comma, do block]
+    parse_defmodule(prog_ast);
 }
 
+pub fn parse_defmodule(ast_node: Pair<Rule>) {
+    for pair in ast_node.into_inner() {
+        match pair.as_rule() {
+            Rule::do_block => parse_do_block(pair),
+            _              => (),
+        };
+    }
+}
 
+pub fn parse_do_block(ast_node: Pair<Rule>) {
+    // Do block contains multiple statements in it's arguments
+
+    for pair in ast_node.into_inner() {
+        match pair.as_rule() {
+            Rule::block => parse_block(pair),
+            _           => (),
+        };
+    }
+}
+
+pub fn parse_block(ast_node: Pair<Rule>) {
+    for pair in ast_node.into_inner() {
+        match pair.as_rule() {
+            Rule::block_statements => parse_block_statements(pair),
+            _                      => (),
+        };
+    }
+    
+}
+
+pub fn parse_block_statements(ast_node: Pair<Rule>) {
+    for pair in ast_node.into_inner() {
+        match pair.as_rule() {
+            Rule::block_statement => parse_block_statement(pair),
+            _                     => (),
+        }
+    }
+}
+
+pub fn parse_block_statement(ast_node: Pair<Rule>) {
+    for pair in ast_node.into_inner() {
+        match pair.as_rule() {
+            Rule::function_definition => parse_function_definition(pair),
+            _                         => (),
+        }
+    }
+}
+
+pub fn parse_function_definition(ast_node: Pair<Rule>) {
+    println!("{}\n", ast_node);
+}
