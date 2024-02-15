1f0b381e9d5a6a801bbad2b71ecca22520b2272f
Matt <matthewneaveis@hotmail.co.uk>
2024-02-15 13:36:50 +0000

    New example for distributed calculator

---
+++ elixir_examples/math/lib/distributed_calc.ex
@@ -0,0 +1,18 @@
+defmodule Student do
+
+  def start do
+    c_p = spawn(Calculator, :add, [])
+    send c_p, {:sum, 10, 12, self()}
+    receive do
+      ans -> IO.puts ans
+    end
+  end
+end
+
+defmodule Calculator do
+  def add do
+    receive do
+      {:sum, x, y, pid} -> send pid, x + y
+    end
+  end
+end
