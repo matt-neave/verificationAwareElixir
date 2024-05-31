5a2d4fb976893e27ce1c8f0048d2a56bb1ff87f4
Matt <matthewneaveis@hotmail.co.uk>
2024-06-01 00:14:14 +0100

    Dining philosophers implementation in Elixir

---
+++ manual_translation/philosophers/lib/philosophers.ex
@@ -0,0 +1,126 @@
+defmodule Table do
+  @spec table_loop() :: :ok
+  def table_loop do
+    receive do
+      {:sit, phil} ->
+        send phil, {:ok}
+        table_loop()
+      {:leave, phil} ->
+        send phil, {:ok}
+        table_loop()
+      {:terminate} -> :ok
+    end
+  end
+end
+
+defmodule Philosopher do
+  @spec start_phil(integer()) :: :ok
+  def start_phil(coordinator) do
+    receive do
+      {:bind, table, lfork, rfork} -> phil_loop(coordinator, table, lfork, rfork)
+    end
+  end
+
+  @spec phil_loop(integer(), integer(), integer(), integer()) :: :ok
+  def phil_loop(coordinator, table, lfork, rfork) do
+    # ... think ... #
+    send table, {:sit, self()}
+    wait()
+    # ... sitting ... #
+    send lfork, {:pickup, self()}
+    wait()
+    send rfork, {:pickup, self()}
+    wait()
+
+    # ... eating ... #
+    send table, {:leave, self()}
+    wait()
+    send lfork, {:putdown, self()}
+    wait()
+    send rfork, {:putdown, self()}
+    wait()
+
+    send coordinator, {:done}
+  end
+
+  @spec wait() :: :ok
+  def wait() do
+    receive do
+      {:ok} -> :ok
+    end
+  end
+end
+
+defmodule Fork do
+  @spec start_fork() :: :ok
+  def start_fork do
+    receive do
+      {:lphil, lphil} ->
+        receive do
+          {:rphil, rphil} -> fork_loop(0, lphil, rphil)
+        end
+    end
+  end
+
+  @spec fork_loop(integer(), integer(), integer()) :: :ok
+  def fork_loop(allocated, lphil, rphil) do
+    # allocated: 0 => none, 1 => left, 2 => right
+    receive do
+      {:pickup, phil} ->
+        if phil == lphil do
+          send phil, {:ok}
+          fork_loop(1, lphil, rphil)
+        else
+          send phil, {:ok}
+          fork_loop(2, lphil, rphil)
+        end
+      {:putdown, phil} ->
+        send phil, {:ok}
+        fork_loop(0, lphil, rphil)
+      {:terminate} ->
+        :ok
+    end
+  end
+end
+
+
+defmodule Coordinator do
+  @vae_init true
+  @spec start() :: :ok
+  def start do
+    n = 4
+
+    table = spawn(Table, :table_loop, [])
+
+    forks = for _ <- 1..n do
+      spawn(Fork, :start_fork, [])
+    end
+
+    phils = for i <- 1..n do
+      spawn(Philosopher, :start_phil, [self()])
+    end
+
+    j = n-1
+    for i <- 0..j do
+      phil = Enum.at(phils,i)
+      lfork = Enum.at(forks,i)
+      r_i = rem(i+1, n)
+      rfork = Enum.at(forks, r_i)
+      send phil, {:bind, table, lfork, rfork}
+      send lfork, {:lphil, phil}
+      send rfork, {:rphil, phil}
+    end
+
+    for i <- 1..n do
+      receive do
+        {:done} -> :ok
+      end
+    end
+    IO.puts "All philosophers have finished eating!"
+
+    for fork <- forks do
+      send fork, {:terminate}
+    end
+    send table, {:terminate}
+  end
+end
--- manual_translation/raft/lib/raft.ex
+++ manual_translation/raft/lib/raft.ex
@@ -5,19 +5,20 @@ defmodule RaftNode do
       {:bind, peers} -> peers
     end
     term = 10 * id
-    node_handler(id, peers, n_peers, 0, term, 0, client)
+    logs = []
+    node_handler(id, peers, n_peers, 0, term, 0, client, logs)
   end
 
-  @spec node_handler(integer(), list(), integer(), atom(), integer(), integer(), integer()) :: :ok
-  defp node_handler(id, peers, n_peers, state, term, vote_count, client) do
+  @spec node_handler(integer(), list(), integer(), atom(), integer(), integer(), integer(), list()) :: :ok
+  defp node_handler(id, peers, n_peers, state, term, vote_count, client, logs) do
     receive do
       {:request_vote, candidate_term, candidate_id, reply_to} ->
         if candidate_term > term do
           send(reply_to, {:vote_granted, id})
-          node_handler(id, peers, n_peers, 0, candidate_term, vote_count, client)
+          node_handler(id, peers, n_peers, 0, candidate_term, vote_count, client, logs)
         else
           send(reply_to, {:vote_denied, id})
-          node_handler(id, peers, n_peers, state, term, vote_count, client)
+          node_handler(id, peers, n_peers, state, term, vote_count, client, logs)
         end
 
       {:vote_granted, _voter_id} ->
@@ -28,16 +29,16 @@ defmodule RaftNode do
             send(peer, {:append_entries, term, id}) # Send log here
           end
           send(client, {:elected, id})
-          node_handler(id, peers, n_peers, 2, term, new_vote_count, client)
+          node_handler(id, peers, n_peers, 2, term, new_vote_count, client, logs)
         else
-          node_handler(id, peers, n_peers, state, term, new_vote_count, client)
+          node_handler(id, peers, n_peers, state, term, new_vote_count, client, logs)
         end
 
       {:start_election} ->
         for peer <- peers do
           send(peer, {:request_vote, term + 1, id, self()})
         end
-        node_handler(id, peers, n_peers, 1, term + 1, 0, client)
+        node_handler(id, peers, n_peers, 1, term + 1, 0, client, logs)
 
       {:terminate} ->
         IO.puts("Terminating node")
